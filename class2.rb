ef7737aeea7fe99ee738b9ff2d57996f64715c67
Patrick Lavender <patrick_lavender@mba.berkeley.edu>
2013-11-15 03:57:24 +0000

    first commit

---
+++ class2.rb
@@ -0,0 +1,13 @@
+class Greeter
+ def initialize(name = "World")
+  @name = name
+ end
+ 
+ def say_hi
+  puts "Hi #{@name}"
+ end
+  
+ def say_bye
+  puts "See ya later #{@name}"
+ end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Demoapp::Application.config.secret_key_base = '449d1a34059803d2dd8520f1befb14ec8209cb3d77b0d4932f66fe32b7ed19eca26c5cb494acec368ed49196c36b6c89550355cb857cc60d6f0d2718bf5193c2'
